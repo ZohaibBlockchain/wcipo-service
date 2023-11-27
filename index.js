075c841dc0710a6007b9e5dde645248db5190525
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-11-27 18:45:12 +0400

    fix

--- index.js
+++ index.js
@@ -120,8 +120,8 @@ async function Login(email, password) {
       if (filteredUsers.length > 0) {
         //send full details of the user
         const cleanProfile = { email: users[0].profile.email, fullName: users[0].profile.fullName, address: users[0].profile.address, phone: users[0].profile.phone, country: users[0].profile.country };
-        const loginData = { token: filteredUsers.token, profile: cleanProfile, works: users[0].works };
-        return { message: "Sign-in successful", success: true, loginData: loginData, expiryTime: filteredUsers[0].expiryTime };
+        const loginData = { token: filteredUsers.token, profile: cleanProfile, works: users[0].works, expiryTime: filteredUsers[0].expiryTime };
+        return { message: "Sign-in successful", success: true, loginData: loginData };
       }
     }
 
@@ -163,8 +163,8 @@ async function Login(email, password) {
             expiryTime: expiryTime,
           });
           const cleanProfile = { email: users[0].profile.email, fullName: users[0].profile.fullName, address: users[0].profile.address, phone: users[0].profile.phone, country: users[0].profile.country };
-          const loginData = { token: token, profile: cleanProfile, works: users[0].works };
-          return { message: "Sign-in successful", success: true, loginData: loginData, expiryTime: expiryTime };
+          const loginData = { token: token, profile: cleanProfile, works: users[0].works , expiryTime: expiryTime};
+          return { message: "Sign-in successful", success: true, loginData: loginData };
         }
       } else {
         return { message: "Password incorrect", success: false };
